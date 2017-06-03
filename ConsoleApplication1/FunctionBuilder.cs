bd9ab2bfb9624400efcb3794c860cf8f303f0df5
Дмитро Гавриляк <dmytro.havryliak@gmail.com>
2017-06-04 01:42:45 +0300

    lab6

---
+++ ConsoleApplication1/FunctionBuilder.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+
+    public class BooleanFunction
+    {
+
+        public List<bool[]> inputs = new List<bool[]>();
+        public List<bool> result = new List<bool>();
+        public string[] variables = new string[] { "x", "y", "z" };
+        public BooleanFunction(bool[][] matrix, bool[] results)
+        {
+            for(int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
+                inputs.Add(matrix[i]);
+
+            result = results.ToList();
+        }
+
+        public bool IsConstantZero()
+        {
+            var row = inputs.First(r => r.All(input => input == false));
+            int index = Array.IndexOf(inputs.ToArray(), row);
+            return result[index] == false ? true : false;
+        }
+
+        public bool IsConstantOne()
+        {
+            var row = inputs.First(r => r.All(input => input == true));
+            int index = Array.IndexOf(inputs.ToArray(), row);
+            return result[index] == true ? true : false;
+        }
+
+        
+        public string ConvertToPDNF()
+        {
+            var dnf = new StringBuilder();
+            List<int> indexes = new List<int>();
+            for(int i = 0; i < result.Count(); i++)
+            {
+                if (result[i] == true)
+                    indexes.Add(i);
+            }
+
+            for (int k = 0; k < indexes.Count(); k++)
+            {
+                var res = String.Empty;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (inputs[indexes[k]][i] == true)
+                    {
+                        res += variables[i];
+                    }
+                    else
+                    {
+                        res += "-";
+                        res += variables[i];
+                    }
+
+                    if (i != 2)
+                        res += "*";
+
+                }
+
+                if (!res.Equals(String.Empty))
+                    dnf.Append("(" + res + ")");
+                if (k != indexes.Count() - 1)
+                    dnf.Append("+");
+            }
+
+            return dnf.ToString();
+        }
+
+        /*public bool CheckSelfDuality()
+        {
+            var functionResults = new List<bool>();
+            var results = new List<bool>();
+            var pdnf = ConvertToPDNF();
+            var expression = new PostfixNotationExpression();
+            string[] postexp = expression.ConvertToPostfixNotation(pdnf);
+
+            foreach (var row in rows)
+            {
+                bool[] array = row.inputs.ToArray();
+
+                for (int i = 0; i < array.Length; i++) {
+                    if (array[i] == true)
+                        array[i] = false;
+                    else
+                        array[i] = true;
+                }
+
+
+                var functionResult = expression.Compute(row.inputs.ToArray(), postexp);
+                var result = !expression.Compute(array, postexp);
+
+                functionResults.Add(functionResult);
+                results.Add(result);
+            }
+
+            for(int i = 0; i < functionResults.Count; i++)
+            {
+                if (functionResults[i] != results[i])
+                    return false;
+            }
+
+            return true;
+
+        }*/
+
+        public string ConvertToPCNF()
+        {
+            var cnf = new StringBuilder();
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < result.Count(); i++)
+            {
+                if (result[i] == false)
+                    indexes.Add(i);
+            }
+
+            for (int k = 0; k < indexes.Count(); k++)
+            {
+                var res = String.Empty;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (inputs[indexes[k]][i] == false)
+                    {
+                        res += variables[i];
+                    }
+                    else
+                    {
+                        res += "-";
+                        res += variables[i];
+                    }
+
+                    if (i != 2)
+                        res += "+";
+
+                }
+
+                if (!res.Equals(String.Empty))
+                    cnf.Append("(" + res + ")");
+                if (k != indexes.Count() - 1)
+                    cnf.Append("*");
+            }
+
+            return cnf.ToString();
+        }
+
+      
+
+    } 
+
+  
+
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount;
+            Console.WriteLine("Please, enter the number of input variables:");
+            bool res = Int32.TryParse(Console.ReadLine(), out amount);
+
+            bool[] results = new bool[8];
+            bool[][] matrix = new bool[8][] { new bool[] { false, false, false},
+                                              new bool[] { false, false, true},
+                                              new bool[] { false, true, false},
+                                              new bool[] { false, true, true},
+                                              new bool[] { true, false, false},
+                                              new bool[] { true, false, true},
+                                              new bool[] { true, true, false},
+                                              new bool[] { true, true, true}
+                                            };
+
+
+            Console.WriteLine("Please, enter the results of boolean function:");
+            for (int i = 0; i < Math.Pow(2, amount); i++)
+            {
+                for (int k = 0; k < amount; k++)
+                    Console.Write((matrix[i][k] == true ? "1" : "0") + " ");
+                
+                Console.Write(": ");
+                int result;
+                res = Int32.TryParse(Console.ReadLine(), out result);
+                bool finalResult = (result == 1 ? true : false);
+                results[i] = finalResult;
+            }
+
+            var boolFunction = new BooleanFunction(matrix, results);
+
+            bool isConstantZero = boolFunction.IsConstantZero();
+            bool isConstantOne = boolFunction.IsConstantOne();
+            Console.WriteLine("is constant zero = " + isConstantZero + ", is constant one = " + isConstantOne);
+
+            var pdnf = boolFunction.ConvertToPDNF();
+            Console.WriteLine("pdnf presentation : " + pdnf);
+
+            var pcnf = boolFunction.ConvertToPCNF();
+            Console.WriteLine("pcnf presentation : " + pcnf);
+
+            /*bool results3 = boolFunction.CheckSelfDuality();
+            Console.WriteLine("has self-duality = " + results3);
+
+           */
+           
+            Console.ReadLine();
+
+        }
+    }
+}
